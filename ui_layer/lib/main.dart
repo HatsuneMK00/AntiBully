5cdcc14bed46dc42120ba037954a88712e80442f
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-01-22 01:03:04 +0800

    update：引入fijkplayer 还未成功

--- ui_layer/lib/main.dart
+++ ui_layer/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fijkplayer/fijkplayer.dart';
 
 void main() => runApp(MyApp());
 
@@ -44,6 +45,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  final FijkPlayer player = FijkPlayer();
   int _counter = 0;
 
   void _incrementCounter() {
@@ -55,6 +57,21 @@ class _MyHomePageState extends State<MyHomePage> {
       // called again, and so nothing would appear to happen.
       _counter++;
     });
+  }
+
+
+  @override
+  void initState() {
+    super.initState();
+    player.setDataSource(
+        "https://sample-videos.com/video123/flv/240/big_buck_bunny_240p_10mb.flv",
+        autoPlay: true);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    player.release();
   }
 
   @override
@@ -74,32 +91,9 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: FijkView(player: player),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
