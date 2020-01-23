ed49880b87be1d8008d3ff0b60c5d10614345c0d
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-01-23 17:35:11 +0800

    fix: 尝试回退frondend的版本

--- ui_layer/lib/main.dart
+++ ui_layer/lib/main.dart
@@ -74,6 +74,7 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
+//        »ØÍË
         child: Column(
           // Column is also a layout widget. It takes a list of children and
           // arranges them vertically. By default, it sizes itself to fit its
