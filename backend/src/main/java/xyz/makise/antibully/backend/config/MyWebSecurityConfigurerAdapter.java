013a054e427f48d61d97b5dd9ab7a8aefc74908d
HatsuneMK00 <10175101106@stu.ecnu.edu.cn>
2020-02-05 14:19:15 +0800

    update: 尝试引入oauth2.0认证

--- backend/src/main/java/xyz/makise/antibully/backend/config/MyWebSecurityConfigurerAdapter.java
+++ backend/src/main/java/xyz/makise/antibully/backend/config/MyWebSecurityConfigurerAdapter.java
@@ -13,6 +13,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import xyz.makise.antibully.backend.service.UserService;
 
 import javax.servlet.ServletException;
@@ -26,13 +28,13 @@ public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter
     @Autowired
     UserService userService;
 
-//    覆写这个函数 提供一个符合要求的UserService类
+    //    覆写这个函数 提供一个符合要求的UserService类
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
-//    覆写这个类 在配置时使用我们自己写的两个类
+    //    覆写这个类 在配置时使用我们自己写的两个类
 //    在拦截了一个url后 这个url需要什么权限
 //    用户是否会被放行
 //    这些逻辑由我们继承的类里的方法决定
@@ -41,7 +43,7 @@ public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter
         http.authorizeRequests()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers("/user/register").permitAll()
-                .anyRequest().hasAnyRole("USER","ADMIN")
+                .anyRequest().hasAnyRole("USER", "ADMIN")
                 .and()
                 .formLogin().loginProcessingUrl("/login")
                 .usernameParameter("username").passwordParameter("password")
@@ -62,15 +64,15 @@ public class MyWebSecurityConfigurerAdapter extends WebSecurityConfigurerAdapter
 //                        这是从response里拿出来的流 没必要关了吧？
 //                        out.close();
                 }).successHandler((request, response, authentication) -> {
-                    response.setContentType("application/json;charset=utf-8");
-                    PrintWriter out = response.getWriter();
-                    StringBuffer stringBuffer = new StringBuffer();
-                    stringBuffer.append("{\"status\":\"success\",\"msg\":\"");
-                    stringBuffer.append("登陆成功");
-                    stringBuffer.append("\"}");
-                    out.write(stringBuffer.toString());
-                    out.flush();
-    //                out.close();
-                }).and().cors().and().logout().permitAll().and().csrf().disable();
+            response.setContentType("application/json;charset=utf-8");
+            PrintWriter out = response.getWriter();
+            StringBuffer stringBuffer = new StringBuffer();
+            stringBuffer.append("{\"status\":\"success\",\"msg\":\"");
+            stringBuffer.append("登陆成功");
+            stringBuffer.append("\"}");
+            out.write(stringBuffer.toString());
+            out.flush();
+//           out.close();
+        }).and().cors().and().logout().permitAll().and().csrf().disable();
     }
 }
